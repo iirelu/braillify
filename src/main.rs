d66cc1e2ee2cc439e71bd836a3ca18be7ae8d5cf
iirelu <anna@bawk.space>
2017-07-23 23:27:16 +0200

    [breaking] Using Clap instead of own code
    There was a lot of code all for the purpose of parsing arguments when
    there's plenty of libraries that do just that. Clap's a bit hefty, but I
    chose it anyway.
    
    Important change in command line syntax: The confusing and complicated
    10x10, 10x, x10, syntax is now gone, and replaced with --width and
    --height flags. If only one is set, the other is inferred from the image
    dimensions using the aspect ratio.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,60 @@
 extern crate image;
+extern crate clap;
 
-use std::env;
 use std::process;
 
 use display::Display;
-use parse_args::ArgParser;
-use parse_args::Error as ParseError;
+use clap::{App, Arg};
 
 mod display;
 mod braille;
-mod parse_args;
-
-
-const USAGE_TEXT: &'static str = "\
-braillify image [size]
-    image: Any image (ex: image.png)
-    size: The desired output size in characters. (ex: 50x25)
-        If a size isn't given, it'll be guessed from the image's dimensions.";
 
 fn main() {
-    let args = match ArgParser::new(env::args().skip(1).collect()) {
-        Err(ParseError::TooFewArgs) =>
-            complain("Too few arguments!"),
-        Err(ParseError::CantParseSize) =>
-            complain("Couldn't parse size!"),
-        Err(ParseError::BadArgs) =>
-            complain("Bad arguments!"),
-        Ok(args) => args
-    };
+    let matches = App::new("braillify")
+        .about("Converts images into braille text")
+        .arg(Arg::with_name("width")
+            .short("w")
+            .long("width")
+            .takes_value(true)
+            .help("Manually specify the width of the generated braille")
+            .long_help(
+"Specifies the width of the rendered braille image. If --height is not also
+specified, it is guessed from aspect ratio."))
+        .arg(Arg::with_name("height")
+            .short("h")
+            .long("height")
+            .takes_value(true)
+            .help("Manually specify the height of the generated braille")
+            .long_help(
+"Specifies the height of the rendered braille image. If --width is not also
+specified, it is guessed from aspect ratio."))
+        .arg(Arg::with_name("file")
+            .required(true)
+            .help("Image file to convert"))
+        .get_matches();
 
-    let img = match image::open(args.path()) {
+    let img = match image::open(matches.value_of("file").unwrap()) {
         Ok(image) => image.to_luma(),
-        Err(_) => complain("Couldn't open image!")
+        Err(_) => {
+            println!("Couldn't open image!");
+            process::exit(1);
+        }
     };
 
-    let (width, height) = args.size().unwrap_or({
-        let (width, height) = img.dimensions();
-        (width/10, height/20)
-    });
+    let (naive_width, naive_height) = img.dimensions();
+    let desired_width: Option<u32> = matches.value_of("width")
+        .map(|x| x.parse().unwrap());
+    let desired_height: Option<u32> = matches.value_of("height")
+        .map(|x| x.parse().unwrap());
+
+    let (width, height) = match (desired_width, desired_height) {
+        (None, None) => (naive_width / 10, naive_height / 20),
+        (Some(w), None) => (w, ((naive_height * w) / naive_width) / 2),
+        (None, Some(h)) => (((naive_width * h) / naive_height) * 2, h),
+        (Some(w), Some(h)) => (w, h)
+    };
 
     let display = Display::new(img, width, height);
 
     print!("{}", display.render());
-}
-
-fn complain(error: &str) -> ! {
-    println!("{}\n\n{}", error, USAGE_TEXT);
-    process::exit(1);
 }
